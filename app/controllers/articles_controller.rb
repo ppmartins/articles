11e9be8617286b08f9cc8d6120303960cebdf0d1
Pedro <ppereiramartins@gmail.com>
2016-05-24 15:42:20 +0100

    crud completed with update and delete actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
     if @article.save
       flash[:notice] = "Your article was saved successfully."
-      redirect_to article_path
+      redirect_to @article
 
     else
       flash[:alert] = "Your article wasn't saved. Please try again."
@@ -27,5 +27,34 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.title = params[:article][:title]
+    @article.body = params[:article][:body]
+
+    if @article.save
+      flash[:notice] = "Your article was updated successfully."
+      redirect_to @article
+
+    else
+      flash[:alert] = "Your article wasn't updated. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      flash[:notice] = "Your article was deleted successfully."
+      redirect_to articles_path
+
+    else
+      flash[:alert] = "Your article wasn't deleted. Please try again."
+      render :show
+    end
   end
 end
