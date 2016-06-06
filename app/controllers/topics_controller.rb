0cfd32cfedd35658fd6d92480ffb9d828614001d
Pedro <ppereiramartins@gmail.com>
2016-06-06 18:50:42 +0100

    Finished the Topic controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,6 +3,10 @@ class TopicsController < ApplicationController
     @topics = Topic.all
   end
 
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
   def new
     @topic = Topic.new
   end
@@ -14,10 +18,40 @@ class TopicsController < ApplicationController
 
     if @topic.save
       flash[:notice] = "Topic was saved successfully."
-      redirect_to topic_articles
+      redirect_to @topic
     else
       flash.now[:alert] = "Error creating the topic. Please try again."
       redirect_to new_topic
     end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error creating the topic. Please try again."
+      redirect_to edit_topic
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+       redirect_to action: :index
+     else
+       flash.now[:alert] = "There was an error deleting the topic."
+       render :show
+     end
   end
 end
