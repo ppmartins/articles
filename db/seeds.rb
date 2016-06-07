f3a5675b1cf9e8db0f78dfbfd162bb05d5134b7c
Pedro <ppereiramartins@gmail.com>
2016-06-07 10:56:27 +0100

    Having routing problems but seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+users = User.all
+
+5.times do
+  Topic.create!(
+    name: Faker::Name.name,
+    description: Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
+10.times do
+  Article.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    topic: topics.sample
+  )
+end
+articles = Article.all
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
+puts "#{Article.count} articles created"
